b5d026480f8d71ed9d62a10cfcbd2d318671a975
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2025-10-14 15:07:29 +0500

    #3. Avtorizatsiya && Middleware Next intl

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { enUS, ruRU, trTR } from '@clerk/localizations'
+import { uzUZ } from './uz-UZ'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs))
+}
+
+export function localization(lng: string) {
+	if (lng === 'en') enUS
+	if (lng === 'ru') ruRU
+	if (lng === 'tr') trTR
+	if (lng === 'uz') uzUZ
 }
